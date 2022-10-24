204b0badc27c9b1dcc61a2dcbcd2b957fcba8143
Irena <ionyanova@mail.ru>
2022-10-24 19:44:16 +0300

    задача рюкзак

--- ans.cpp
+++ ans.cpp
@@ -1,42 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <string>
-
+#include <algorithm>
 using namespace std;
-
-int min(int a, int b)
-{
-    return a > b ? b : a;
-}
 
 int main()
 {
-    string Str1, Str2;
+	unsigned int capacity, num;
+	cin >> capacity >> num;//вместимость рюкзака, число золотых слитков
+	vector<int> weights(num);//веса золотых слитков
+	for (int j = 0; j < num; ++j) cin >> weights[j];
 
-    cin >> Str1;
-    cin >> Str2;
-
-    vector<int> Current, Prev;//текущий, предыдущий
-
-    Current.resize(Str1.length() + 1);//размер вектора на ячейку больше строки
-    Prev.resize(Str1.length() + 1);//размер вектораа на ячейку больше строки
-
-    for (int i = 0; i < Current.size(); ++i)
-        Prev[i] = i;//->от 0 до 5
-
-    for (int i = 1; i < Str2.length() + 1; ++i)
-    {
-        Current[0] = i;
-        for (int j = 1; j < Current.size(); ++j)
-        {
-            int Del = Current[j - 1] + 1, Repl = 1 - (Str1[j - 1] == Str2[i - 1]) + Prev[j - 1], Add = Prev[j] + 1;
-
-            Current[j] = min(Del, min(Repl, Add));//инициализируем ячейку минимальным значением
-        }
-        Prev = Current;
-    }
-
-    cout << Current[Current.size() - 1];
-
-    return 0;
+	vector<int> prev(capacity + 1, 0);//предыдущий
+	vector<int> current(capacity + 1);//текущий
+	for (int i = 0; i < num; ++i) {
+		current[0] = 0;
+		for (int j = 1; j < current.size(); ++j) {
+			if (weights[i] <= j) current[j] = max(prev[j], prev[j - weights[i]] + weights[i]);
+			else current[j] = prev[j];
+		}
+		current.swap(prev);//переставляем текущий на предыдущий
+	}
+	cout << prev[capacity];//макс вес, который можно унести в рюкзаке
+	return 0;
 }
+
