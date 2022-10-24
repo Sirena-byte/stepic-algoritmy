abd43125cb35cc83e2a1403fd88d3d3a41e80d9e
Irena <ionyanova@mail.ru>
2022-10-24 16:34:47 +0300

    добавлено решение задачи 8.3 расстояние редактирования

--- ans.cpp
+++ ans.cpp
@@ -1,59 +1,42 @@
-#include <iostream>         //mid- индекс текущего эл
-#include <vector>           //pos-
-                            //low- длинаа последовательности
-#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
 
-template <class Type>
-std::vector<int> LNIS_bottom_up_bs(const std::vector<Type>& A) {//формируется неубывающая последовательность
-
-    int n = A.size(), len = 0;
-    std::vector<int> D(n + 1), P(n);//создаем два вектора 
-
-    // current <= last: Append current element
-    // current > last : Replace it with current element
-    //нахождение длины последовательности
-    for (int i = 0; i < n; ++i) {
-
-        int low = 1;//
-        int high = len;//
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (A[D[mid]] >= A[i])
-                low = mid + 1;//если текущий меньше или равен последнему,добавить текущий элемент
-            else 
-                high = mid - 1;
-        }
-        int pos = low;//текущий
-        // Parent (backtrack)
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        P[i] = D[pos - 1];
-        D[pos] = i;
-        if (pos > len) len = pos;//текущий > последний : заменить его текущим элементом
-    }
-    int k = D[len];
-    std::vector<int> seq(len);
-    for (int i = 0; i < len; ++i) {
-        seq[len - 1 - i] = k + 1;//запонляется с конца
-        k = P[k];
-    }
-
-    return seq;
+int min(int a, int b)
+{
+    return a > b ? b : a;
 }
 
-int main() {//ЗАПОЛНЯЕМ ВЕКТОР ЧИСЛАМИ
-    int n;
-    std::cin >> n;
-    std::vector<int> A(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> A[i];
+int main()
+{
+    string Str1, Str2;
+
+    cin >> Str1;
+    cin >> Str2;
+
+    vector<int> Current, Prev;//текущий, предыдущий
+
+    Current.resize(Str1.length() + 1);//размер вектора на ячейку больше строки
+    Prev.resize(Str1.length() + 1);//размер вектораа на ячейку больше строки
+
+    for (int i = 0; i < Current.size(); ++i)
+        Prev[i] = i;//->от 0 до 5
+
+    for (int i = 1; i < Str2.length() + 1; ++i)
+    {
+        Current[0] = i;
+        for (int j = 1; j < Current.size(); ++j)
+        {
+            int Del = Current[j - 1] + 1, Repl = 1 - (Str1[j - 1] == Str2[i - 1]) + Prev[j - 1], Add = Prev[j] + 1;
+
+            Current[j] = min(Del, min(Repl, Add));//инициализируем ячейку минимальным значением
+        }
+        Prev = Current;
     }
 
-    std::vector<int> seq = LNIS_bottom_up_bs(A);
-    std::cout << seq.size() << std::endl;//длина невозрастающая послеедовательности
-    for (size_t i = 0; i < seq.size(); ++i) {
-        std::cout << seq[i] << " ";//последовательность
-    }
+    cout << Current[Current.size() - 1];
 
     return 0;
 }
