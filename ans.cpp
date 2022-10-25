85ab245d2fbe4168d711be8138356878ab83014d
Irena <ionyanova@mail.ru>
2022-10-25 08:02:34 +0300

    задача лестница

--- ans.cpp
+++ ans.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 int main()
 {
-	unsigned int capacity, num;
-	cin >> capacity >> num;//вместимость рюкзака, число золотых слитков
-	vector<int> weights(num);//веса золотых слитков
-	for (int j = 0; j < num; ++j) cin >> weights[j];
-
-	vector<int> prev(capacity + 1, 0);//предыдущий
-	vector<int> current(capacity + 1);//текущий
-	for (int i = 0; i < num; ++i) {
-		current[0] = 0;
-		for (int j = 1; j < current.size(); ++j) {
-			if (weights[i] <= j) current[j] = max(prev[j], prev[j - weights[i]] + weights[i]);
-			else current[j] = prev[j];
-		}
-		current.swap(prev);//переставляем текущий на предыдущий
+	int count;
+	cin >> count;
+	int a = 0, b = 0;
+	for (int i = 0; i < count; ++i)
+	{
+		int tmp = b;
+		int nom = 0;
+		cin >> nom;
+		b = std::max(a, b) + nom;
+		a = tmp;
 	}
-	cout << prev[capacity];//макс вес, который можно унести в рюкзаке
+
+	cout << b;
+
 	return 0;
 }
-
